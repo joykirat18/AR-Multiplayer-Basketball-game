4a2cf27d926d595ed089c0b9635a825cca91dda4
joykirat18 <joykiratsingh18@gmail.com>
2021-04-18 19:58:15 +0530

    placed all the assets

---
+++ Assets/Basket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+public class Basket : MonoBehaviour
+{
+	// public GameObject score;
+	public AudioClip basket;
+	public Text ScoreText;
+	void OnCollisionEnter()
+	{
+		GetComponent<AudioSource>().Play();
+	}
+
+	void OnTriggerEnter()
+	{
+		int currentScore = int.Parse(buttonQueries.ScoreText.text) + 1;
+		buttonQueries.ScoreText.text = currentScore.ToString();
+		AudioSource.PlayClipAtPoint(basket, transform.position);
+	}
+}
---
+++ Assets/buttonQueries.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonQueries : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject PlacementIndicator;
+	public GameObject basketBallRing;
+	public GameObject StartButton;
+	// public static Text ScoreText;
+
+	private PlacementIndicator placementIndicator;
+	void Start()
+	{
+
+	}
+
+	public void onStart()
+	{
+		placementIndicator = FindObjectOfType<PlacementIndicator>();
+		GameObject obj = Instantiate(basketBallRing, placementIndicator.transform.position, placementIndicator.transform.rotation);
+		Destroy(PlacementIndicator);
+		StartButton.SetActive(false);
+	}
+
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
